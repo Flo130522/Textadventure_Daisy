25e67cbbc811c56aeec10299190c594779903016
Flo <144101306+Flo130522@users.noreply.github.com>
2023-10-04 16:05:38 +0200

    Issue #71  - Erledigt

---
+++ ascii.py
@@ -0,0 +1,4 @@
+import art
+ascii_art = art.text2art("Das Abenteuer des Rachedackels", font="banner3")
+ascii_art = ascii_art + "\n" + art.text2art("Daisy gegen Hubertus Snickers", font="banner")
+print(ascii_art)
